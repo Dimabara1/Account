8074ef7c5514c664b1bb491085c1551250d4289d
Dimabara1 <Dzmitrybarabanou@gmail.com>
2023-03-13 09:00:41 +0300

    First_commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿class Account
+{
+    private string name;
+    private int account_number;
+    private double amount;
+
+    public string Name { get => name; set => name = value; }
+    public int Account_number { get => account_number; set => account_number = value; }
+    public double Amount { get => amount; set => amount = value; }
+}
